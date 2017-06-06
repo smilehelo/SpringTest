8e540523902d78eb47403d9389b01fbac83c254b
smile_helo <smile_helo@163.com>
2017-06-07 00:30:46 +0800

    no message

--- src/main/java/me/springtest/controller/UploadCtr.java
+++ src/main/java/me/springtest/controller/UploadCtr.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,15 +19,23 @@ public class UploadCtr {
 	}
 	
 	
+	/**
+	 * 上传多个文件的并保存
+	 * @param upload
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
 	@RequestMapping("/upload")
 	public void upload(@RequestPart("upload")MultipartFile[] upload) throws IllegalStateException, IOException{
 		System.out.println(upload.length);
 		for(MultipartFile one : upload){
+			//如果没有文件，不执行
+			if(StringUtils.isEmpty(one.getOriginalFilename())){
+				continue;
+			}
 			System.out.println(one.getOriginalFilename());
 			System.out.println(one.getSize());
 			one.transferTo(new File(System.currentTimeMillis()+one.getOriginalFilename()));
 		}
-		
-
 	}
 }
